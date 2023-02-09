51c43e5fa56de919d314b690e8b53fc244ce2f18
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2023-02-09 17:52:21 -0300

    Feature: Requisite 01 completed

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,9 +2,57 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Login extends Component {
+  state = {
+    disabled: true,
+    emailValue: '',
+    nameValue: '',
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => {
+      const { emailValue, nameValue } = this.state;
+      if (emailValue.length > 0 && nameValue.length > 0) {
+        this.setState({
+          disabled: false,
+        });
+      }
+    });
+  };
+
+  handleClick = () => {
+
+  };
+
   render() {
+    const { disabled } = this.state;
     return (
-      <div>Login</div>
+      <form>
+        <input
+          data-testid="input-gravatar-email"
+          placeholder="Insira o seu email"
+          name="emailValue"
+          onChange={ this.handleChange }
+        />
+        <input
+          data-testid="input-player-name"
+          placeholder="Insira o seu nome"
+          onChange={ this.handleChange }
+          name="nameValue"
+        />
+
+        <button
+          type="button"
+          data-testid="btn-play"
+          disabled={ disabled }
+          onClick={ this.handleClick }
+        >
+          Play
+
+        </button>
+      </form>
     );
   }
 }
