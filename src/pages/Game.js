85d0df5c1c96fabfb09b7db369f1f72641efd15c
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2023-02-10 17:53:53 -0300

    Feature: Requisite 06 completed

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,15 +1,111 @@
 import { connect } from 'react-redux';
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Game extends Component {
+  state = {
+    amount: 5,
+    questions: [],
+    currentQuestion: 0,
+    correctAnswer: '',
+  };
+
+  componentDidMount() {
+    this.fetchAPIGame();
+  }
+
+  fetchAPIGame = async () => {
+    const { amount } = this.state;
+    const token = localStorage.getItem('token');
+    const URL_GAME_BASIC = `https://opentdb.com/api.php?amount=${amount}&token=${token}`;
+    const response = await fetch(URL_GAME_BASIC);
+    const returnAPI = await response.json();
+    const CODE_ERROR = 3;
+    if (returnAPI.response_code === CODE_ERROR) {
+      const { history } = this.props;
+      history.push('/');
+    }
+    this.setState({
+      questions: returnAPI.results,
+    }, () => {
+      this.handleCorrectAnswer();
+    });
+  };
+
+  handleClick = () => {
+    const { currentQuestion, questions } = this.state;
+    const maxQuestions = questions.length;
+    const { history } = this.props;
+    if (currentQuestion + 1 === maxQuestions) {
+      history.push('/feedback');
+    } else {
+      this.setState({ currentQuestion: currentQuestion + 1 }, () => {
+        this.handleCorrectAnswer();
+      });
+    }
+  };
+
+  handleCorrectAnswer = () => {
+    const { questions, currentQuestion } = this.state;
+    this.setState({
+      correctAnswer: questions[currentQuestion].correct_answer,
+    });
+  };
+
   render() {
+    const { questions, currentQuestion, correctAnswer } = this.state;
+    let counter = 0;
+    let dataID = '';
+    let answers = [];
+    if (questions.length >= 1) {
+      answers = [
+        questions[currentQuestion].correct_answer,
+        ...questions[currentQuestion].incorrect_answers];
+      const randonator = 0.5;
+      answers.sort(() => Math.random() - randonator);
+    }
     return (
-      <div>
-        <Header />
-      </div>
+
+      questions.length >= 1 && (
+        <div>
+          <Header />
+          <main>
+            <h5 data-testid="question-text">{questions[currentQuestion].question}</h5>
+            <h5 data-testid="question-category">{questions[currentQuestion].category}</h5>
+            <section data-testid="answer-options">
+              {answers.map((answer, index) => {
+                if (answer === correctAnswer) {
+                  dataID = 'correct-answer';
+                } else {
+                  dataID = `wrong-answer-${counter}`;
+                  counter += 1;
+                }
+
+                return (
+                  <button
+                    key={ index }
+                    type="button"
+                    data-testid={ dataID }
+                  >
+                    {answer}
+                  </button>
+                );
+              })}
+            </section>
+            <button type="button" onClick={ this.handleClick }>Next</button>
+          </main>
+        </div>
+      )
+
     );
   }
 }
+
+Game.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default connect()(Game);
