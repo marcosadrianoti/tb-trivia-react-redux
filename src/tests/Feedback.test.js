c57a8c84791df14a485a58aa9d1e616467c2726e
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-02-14 15:30:55 -0300

    Refactor: Requisite 17 - Buttons

--- src/tests/Feedback.test.js
+++ src/tests/Feedback.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
 import Feedback from '../pages/Feedback';
+import App from '../App';
 
 describe('Testando a página Feedback', () => {
   test('Ao acessar diretamente a rota /feedback, o score, score total e questões certas serão 0', () => {
@@ -29,4 +31,56 @@ describe('Testando a página Feedback', () => {
     const img = screen.getByTestId('header-profile-picture');
     expect(img).toBeInTheDocument();
   });
+  test('Testando se o botão Play-Again funciona', () => {
+    const initialState = {};
+    const route = '/feedback';
+    const { history } = renderWithRouterAndRedux(<App />, initialState, route );
+    expect( history.location.pathname ).toBe('/feedback');
+
+    const btnPlayAgain = screen.getByTestId('btn-play-again');
+    expect(btnPlayAgain).toBeInTheDocument();
+    userEvent.click(btnPlayAgain);
+    expect(history.location.pathname).toBe('/');
+  })
+  test('Testando se o botão Ranking funciona', () => {
+    const initialState = {};
+    const initialStateData = {
+      ranking: [
+        {
+          name: "Bruno",
+          assertions: 2,
+          score: 165,
+          gravatarEmail: "bruno@gmail.com"
+        }
+      ]
+    };
+    localStorage.setItem("Ranking", JSON.stringify(initialStateData.ranking));
+    const route = '/feedback';
+    const { history } = renderWithRouterAndRedux(<App />, initialState, route );
+    expect( history.location.pathname ).toBe('/feedback');
+
+    const btnRanking = screen.getByTestId('btn-ranking');
+    expect(btnRanking).toBeInTheDocument();
+    userEvent.click(btnRanking);
+    expect(history.location.pathname).toBe('/ranking');
+  })
+  test('Testando se a mensagem muda de acordo com os acertos', () => {
+    const initialState = {
+      player: 
+        {
+          name: "Bruno",
+          assertions: 3,
+          score: 165,
+          gravatarEmail: "bruno@gmail.com"
+        }
+    };
+    const route = '/feedback';
+    const { history } = renderWithRouterAndRedux(<App />, initialState, route );
+    expect( history.location.pathname ).toBe('/feedback');
+
+    const msg = screen.getByTestId('feedback-text');
+    expect(msg).toBeInTheDocument();
+    expect(msg.innerHTML).toBe('Well Done!');
+
+  })
 });
